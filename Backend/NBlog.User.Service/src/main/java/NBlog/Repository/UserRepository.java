d087f1e1fd515092e7d304d443e9390d4f9148c7
anjakin <anja261295@gmail.com>
2019-04-03 17:08:27 +0200

    some changes to database settings

--- Backend/NBlog.User.Service/src/main/java/NBlog/Repository/UserRepository.java
+++ Backend/NBlog.User.Service/src/main/java/NBlog/Repository/UserRepository.java
@@ -3,9 +3,11 @@ package NBlog.Repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 import NBlog.DataModel.User;
 
+@Repository
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 	
 	User findUser(@Param("name") String name);
